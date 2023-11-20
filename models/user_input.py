2480fd46336a8c937dc6493e0f2ec02ffd0bd8a7
Ashish Raj A Singh <ashishraj.a@greenkogroup.com>
2023-11-20 15:43:00 +0530

    7

--- models/user_input.py
+++ models/user_input.py
@@ -5,4 +5,3 @@ class DocumentModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     content = db.Column(db.LargeBinary)
-    new_field = db.Column(db.String(50))
