80d3b1825830765304f0b51ffe727361f5832241
Ashish Raj A Singh <ashishraj.a@greenkogroup.com>
2023-12-07 23:18:50 +0530

    changes4

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
+
+from config.config import Config
 from db import db
 from blocklist import BLOCKLIST
 from dotenv import load_dotenv
@@ -17,17 +19,8 @@ from resources.user_input import blp as DocumentBlueprint
 def create_app(db_url=None):
     app = Flask(__name__)
     load_dotenv()
-    app.config["API_TITLE"] = "Stores REST API"
-    app.config["API_VERSION"] = "v1"
-    app.config["OPENAPI_VERSION"] = "3.0.3"
-    app.config["OPENAPI_URL_PREFIX"] = "/"
-    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-    app.config[
-        "OPENAPI_SWAGGER_UI_URL"
-    ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config.from_object(Config)
+
     db.init_app(app)
     migrate = Migrate(app, db)
     api = Api(app)
---
+++ config/config.py
@@ -0,0 +1,19 @@
+import os
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read(os.path.join(os.path.dirname(__file__), 'config.ini'))
+
+
+class Config:
+    API_TITLE = config.get('Flask', 'API_TITLE', fallback="Stores REST API")
+    API_VERSION = config.get('Flask', 'API_VERSION', fallback="v1")
+    OPENAPI_VERSION = config.get('Flask', 'OPENAPI_VERSION', fallback="3.0.3")
+    OPENAPI_URL_PREFIX = config.get('Flask', 'OPENAPI_URL_PREFIX', fallback="/")
+    OPENAPI_SWAGGER_UI_PATH = config.get('Flask', 'OPENAPI_SWAGGER_UI_PATH', fallback="/swagger-ui")
+    OPENAPI_SWAGGER_UI_URL = config.get('Flask', 'OPENAPI_SWAGGER_UI_URL',
+                                        fallback="https://cdn.jsdelivr.net/npm/swagger-ui-dist/")
+    SQLALCHEMY_TRACK_MODIFICATIONS = config.getboolean('Flask', 'SQLALCHEMY_TRACK_MODIFICATIONS', fallback=False)
+    PROPAGATE_EXCEPTIONS = config.getboolean('Flask', 'PROPAGATE_EXCEPTIONS', fallback=True)
+    JWT_SECRET_KEY = config.get('Flask', 'JWT_SECRET_KEY', fallback="jose")
+    SQLALCHEMY_DATABASE_URI = config.get('Flask', 'SQLALCHEMY_DATABASE_URI', fallback="sqlite:///data.db")
